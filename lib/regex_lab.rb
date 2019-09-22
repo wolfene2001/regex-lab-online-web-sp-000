c95a04864860779805c4388916708acd99d696c6
wolfene2001 <wolfene2001@yahoo.com>
2019-09-22 23:45:21 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
 def starts_with_a_vowel?(word)
 
+   if match(/\b[aeiou]*\b/)
+    return true
+  elsif match(/\b[bcdfghjklmnpqrstvwxyz]*\b/)
+    return false
+end
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if
 end
 
 def words_five_letters_long(text)
